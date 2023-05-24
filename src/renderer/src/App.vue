dcc02d5c36293de7807a20fcc627175eec10fb7c
曾经沧海 <2426502977@qq.com>
2023-05-24 22:47:57 +0800

    初始化
    初始化

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Header from './layout/Header.vue'
+import { ElConfigProvider } from 'element-plus'
+
+const zIndex = 3000
+const size = 'small'
+</script>
+
+<template>
+  <el-config-provider :size="size" :z-index="zIndex">
+    <Header />
+    <div>123</div>
+  </el-config-provider>
+</template>
+
+<style lang="less">
+@import './assets/css/styles.less';
+</style>
---
+++ src/renderer/src/layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <el-button type="primary">123</el-button>
+    123
+  </div>
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style scoped>
+.header {
+  width: 100vw;
+  height: 60px;
+  background: rgb(236, 65, 65);
+}
+</style>
